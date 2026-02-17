70f3aaeaf4f70945e03600d120ef18d21591546b
Troy <troyd9216@gmail.com>
2026-02-17 11:31:18 +0900

    Fix: Add all Vite HMR constants to resolve __SERVER_HOST__ error

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,13 @@ export default defineConfig({
     __DEFINES__: "{}",
     __HMR_CONFIG_NAME__: '"import.meta.hot"',
     __BASE__: '"/"',
+    __SERVER_HOST__: '"localhost"',
+    __HMR_PROTOCOL__: "null",
+    __HMR_HOSTNAME__: "null",
+    __HMR_PORT__: "null",
+    __HMR_DIRECT_TARGET__: "null",
+    __HMR_BASE__: '"/"',
+    __HMR_TIMEOUT__: "30000",
+    __HMR_ENABLE_OVERLAY__: "false",
   },
 });
